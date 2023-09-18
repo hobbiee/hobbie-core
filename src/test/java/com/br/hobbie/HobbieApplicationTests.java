45627cc5b3c25c6b7d2f12b2a690aea89806cc0b
lucasqueiroz <sepulchrolucas@gmail.com>
2023-09-17 22:56:23 -0300

    initial commit
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

---
+++ src/test/java/com/br/hobbie/HobbieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.hobbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
