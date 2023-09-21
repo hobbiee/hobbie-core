32e10b76ce15cb4caba7bdc46d1e56baf5e13bc4
lucasqueiroz <sepulchrolucas@gmail.com>
2023-09-20 23:40:28 -0300

    test: `CreatePlayer` test file
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

---
+++ src/test/java/com/br/hobbie/modules/player/infrastructure/http/controllers/CreatePlayerTest.java
@@ -0,0 +1,12 @@
+package com.br.hobbie.modules.player.infrastructure.http.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CreatePlayerTest {
+
+    @Test
+    @DisplayName("Should create a player with success")
+    void createPlayerSuccessfully() {
+    }
+}
