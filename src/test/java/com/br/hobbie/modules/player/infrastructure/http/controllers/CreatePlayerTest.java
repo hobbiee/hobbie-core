348097690e6d68bcd6ebd0a20ffaac17c10f6bf6
lucasqueiroz <sepulchrolucas@gmail.com>
2023-09-22 16:59:34 -0300

    test(`create-player`): test case for validations error when create a player.
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/test/java/com/br/hobbie/modules/player/infrastructure/http/controllers/CreatePlayerTest.java
+++ src/test/java/com/br/hobbie/modules/player/infrastructure/http/controllers/CreatePlayerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Map;
 
@@ -40,7 +42,25 @@ class CreatePlayerTest {
 
     @Test
     @DisplayName("Should not create a player with invalid data")
-    void createPlayerWithInvalidData() {
+    void createPlayer_ReturnsError_WithInvalidData() throws Exception {
+        // GIVEN
+        Map<String, Object> params = Map.of(
+                "name", "",
+                "avatar", "https://www.google.com",
+                "latitude", 10,
+                "longitude", 10,
+                "birthDate", "1990-01-01",
+                "interests", new String[]{"soccer", "basketball"}
+        );
+
+        // WHEN
+        var result = mvc.post(URL, params);
+
+        // THEN
+        result.andExpect(mvc.status().isBadRequest());
+        result.andExpect(mvc.status().is(HttpStatus.BAD_REQUEST.value()));
+        MockMvcResultMatchers.jsonPath("$.errors").exists();
+        MockMvcResultMatchers.jsonPath("$.errors").isNotEmpty();
     }
 
 
