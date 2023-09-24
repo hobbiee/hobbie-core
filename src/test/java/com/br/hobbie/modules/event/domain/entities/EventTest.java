a33e2053b476b4ff77fa27330b7bc3dc46c5ee01
lucasqueiroz <sepulchrolucas@gmail.com>
2023-09-24 12:11:44 -0300

    feat: adds first test class for `event` and auxiliar factory for tests
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

---
+++ src/test/java/com/br/hobbie/modules/event/domain/entities/EventTest.java
@@ -0,0 +1,111 @@
+package com.br.hobbie.modules.event.domain.entities;
+
+import com.br.hobbie.modules.player.domain.entities.Player;
+import com.br.hobbie.shared.core.errors.Either;
+import com.br.hobbie.shared.factory.PlayerEventTestFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class EventTest {
+    static Event event;
+
+    @BeforeAll
+    static void setUp() {
+        event = PlayerEventTestFactory.createEvent();
+    }
+
+    @Test
+    @DisplayName("Should add participant if capacity is not reached")
+    void addParticipant_WhenCapacityNotReached() {
+        // GIVEN
+        Player player = PlayerEventTestFactory.createParticipant();
+
+        // WHEN
+        event.addParticipant(player);
+
+        // THEN
+        // two participants because the admin is also a participant
+        Assertions.assertEquals(2, event.getParticipants().size());
+        Assertions.assertTrue(event.getParticipants().contains(player));
+    }
+
+    @Test
+    @DisplayName("Should not add participant if capacity is reached")
+    void addParticipant_WhenCapacityReached() {
+        // GIVEN
+        Player player = PlayerEventTestFactory.createParticipant();
+        for (int i = 0; i < 10; i++) {
+            event.addParticipant(PlayerEventTestFactory.createParticipant());
+        }
+
+        // WHEN
+        Either<RuntimeException, Boolean> result = event.addParticipant(player);
+
+        // THEN
+        Assertions.assertTrue(result.isLeft());
+        Assertions.assertEquals("Event is full", result.getLeft().getMessage());
+    }
+
+    @Test
+    @DisplayName("Should not add participant if the player is the admin")
+    void addParticipant_WhenPlayerIsAdmin() {
+        // GIVEN
+        Player player = event.getAdmin();
+
+        // WHEN
+        Either<RuntimeException, Boolean> result = event.addParticipant(player);
+
+        // THEN
+        Assertions.assertTrue(result.isLeft());
+        Assertions.assertEquals("Player is already admin of this event", result.getLeft().getMessage());
+        Assertions.assertEquals(1, event.getParticipants().size());
+    }
+
+    @Test
+    void close() {
+    }
+
+    @Test
+    void getAdmin() {
+    }
+
+    @Test
+    @DisplayName("Should return true if capacity is reached")
+    void isFull() {
+        // GIVEN
+        for (int i = 0; i < 10; i++) {
+            event.addParticipant(PlayerEventTestFactory.createParticipant());
+        }
+
+        // WHEN
+        boolean result = event.capacityReached();
+
+        // THEN
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Should return false if has capacity")
+    void isNotFull() {
+        // GIVEN
+        // 8 participants because the admin is also a participant
+        for (int i = 0; i < 8; i++) {
+            event.addParticipant(PlayerEventTestFactory.createParticipant());
+        }
+
+        // WHEN
+        boolean result = event.capacityReached();
+
+        // THEN
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void getName() {
+    }
+}
