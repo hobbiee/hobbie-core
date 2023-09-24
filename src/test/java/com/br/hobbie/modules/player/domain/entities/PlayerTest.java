4b496f49d35755e48a5a3effcfaeed5e127c245d
lucasqueiroz <sepulchrolucas@gmail.com>
2023-09-24 12:15:38 -0300

    refactor: removes redundant validation
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/test/java/com/br/hobbie/modules/player/domain/entities/PlayerTest.java
+++ src/test/java/com/br/hobbie/modules/player/domain/entities/PlayerTest.java
@@ -207,20 +207,6 @@ class PlayerTest {
         Assertions.assertTrue(result.isRight());
     }
 
-    @Test
-    @DisplayName("Should not be able to join an event if is not active")
-    void joinEvent_ReturnsEitherErrorWhenNotActive() {
-        // GIVEN - setUp
-        Player other = new Player("other name", "avatar", BigDecimal.ONE, BigDecimal.TEN, LocalDate.of(1999, 1, 1));
-        player.createEvent(event);
-        event.close();
-
-        // WHEN
-        Either<RuntimeException, Boolean> result = other.joinEvent(event);
-
-        // THEN
-        Assertions.assertTrue(result.isLeft());
-    }
 
     @Test
     @DisplayName("Should not be able to join an event if is admin")
@@ -230,24 +216,6 @@ class PlayerTest {
 
         // WHEN
         Either<RuntimeException, Boolean> result = player.joinEvent(event);
-
-        // THEN
-        Assertions.assertTrue(result.isLeft());
-    }
-
-    @Test
-    @DisplayName("Should not be able to join an event if event is full")
-    void joinEvent_ReturnsEitherErrorWhenEventIsFull() {
-        // GIVEN - setUp
-        Player other = new Player("other name", "avatar", BigDecimal.ONE, BigDecimal.TEN, LocalDate.of(1999, 1, 1));
-        player.createEvent(event);
-        for (int i = 0; i < CAPACITY; i++) {
-            Player player = new Player("name" + i, "avatar", BigDecimal.ONE, BigDecimal.TEN, LocalDate.of(1999, 1, 1));
-            event.addParticipant(player);
-        }
-
-        // WHEN
-        Either<RuntimeException, Boolean> result = other.joinEvent(event);
 
         // THEN
         Assertions.assertTrue(result.isLeft());
