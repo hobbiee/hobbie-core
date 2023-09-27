633f01acb113e48f78325855cf125b0485046baf
lucasqueiroz <sepulchrolucas@gmail.com>
2023-09-26 21:42:10 -0300

    refactor: split the validator variables to better understanding
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/br/hobbie/shared/core/validators/ValidDateValidator.java
+++ src/main/java/com/br/hobbie/shared/core/validators/ValidDateValidator.java
@@ -22,14 +22,23 @@ public class ValidDateValidator implements ConstraintValidator<ValidDate, DateTi
         }
 
         // if the date is today, the start time must be after now and the end time must be after the start time
-        if (LocalDate.now().equals(object.getDate())) {
-            return (object.getStartTime().isAfter(LocalTime.now())
-                    && object.getEndTime().isAfter(object.getStartTime())) && object.getStartTime().isBefore(LocalTime.of(23, 0, 0));
+        var today = LocalDate.now();
+
+        var hoursNow = LocalTime.of(LocalTime.now().getHour(), LocalTime.now().getMinute());
+
+        var elevenPm = LocalTime.of(23, 0, 0);
+
+        if (today.equals(object.getDate())) {
+            return (object.getStartTime().isAfter(hoursNow)
+                    && object.getEndTime().isAfter(object.getStartTime())) && object.getStartTime().isBefore(elevenPm);
         }
 
         // if date is in the future, both end date and start date can be after now
         // but the end date still cannot be before the start date
-        return object.getEndTime().isAfter(object.getStartTime()) && object.getStartTime().isBefore(LocalTime.of(23, 0, 0));
+        return object
+                .getEndTime()
+                .isAfter(object.getStartTime()) &&
+                object.getStartTime().isBefore(elevenPm);
     }
 }
 
