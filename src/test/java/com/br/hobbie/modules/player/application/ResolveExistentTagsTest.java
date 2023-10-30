24b9e9558347c7d792788e1725b821f4e398422e
lucasqueiroz <sepulchrolucas@gmail.com>
2023-10-30 10:45:40 -0300

    [Tags]: makes the `ResolveExistentTags` use-case more clean
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/test/java/com/br/hobbie/modules/player/application/ResolveExistentTagsTest.java
+++ src/test/java/com/br/hobbie/modules/player/application/ResolveExistentTagsTest.java
@@ -1,83 +1,60 @@
 package com.br.hobbie.modules.player.application;
 
 import com.br.hobbie.modules.player.domain.entities.Tag;
-import com.br.hobbie.modules.player.domain.repositories.TagRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = "spring.profiles.active=test"
-)
+import java.util.stream.Stream;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = "spring.profiles.active=test")
 class ResolveExistentTagsTest {
+
+    private static final Tag soccer = new Tag("soccer");
+    private static final Tag football = new Tag("football");
+    private static final Tag basketball = new Tag("basketball");
+
 
     @Autowired
     private ResolveExistentTags resolveExistentTags;
 
     @Autowired
-    private TagRepository tagRepository;
-
-    private Tag[] tags;
-
+    private EntityManager manager;
 
     @BeforeEach
     void setUp() {
-        tags = new Tag[]{
-                new Tag("tag with spaces"),
-                new Tag("tag_without_spaces")
-        };
+        manager.persist(soccer);
+        manager.persist(football);
+        manager.persist(basketball);
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        manager.remove(soccer);
+        manager.remove(football);
+        manager.remove(basketball);
     }
 
     @Test
-    @DisplayName("Should resolve existent tags and return a set of them")
-    void resolve_ExistentTags_ShouldReturnASetOfTags() {
+    @DisplayName("Should return an existent tag when trying to add a tag that already exists")
+    @Transactional
+    void should_ReturnAnExistentTag_When_TryingToAddATagThatAlreadyExists() {
         // GIVEN
-        var firstTagSaved = tagRepository.save(tags[0]);
-        tagRepository.save(tags[1]);
+        var tags = new Tag[]{soccer, football, basketball};
 
         // WHEN
-        var result = resolveExistentTags.resolve(tags);
+        var existentTags = resolveExistentTags.resolve(tags);
 
         // THEN
-        Assertions.assertTrue(result.contains(tags[0]));
-        Assertions.assertTrue(result.contains(tags[1]));
-        Assertions.assertEquals(2, result.size());
-        Assertions.assertEquals(firstTagSaved.getId(), result.stream().findFirst().get().getId());
-        Assertions.assertNotNull(result.stream().findFirst().get().getId());
+        Assertions.assertEquals(3, existentTags.size());
+        Stream.of(tags).forEach(tag -> Assertions.assertNotNull(tag.getId()));
     }
 
-    @Test
-    @DisplayName("Should create a new tag and return a set of the existent tags and the new one")
-    void resolve_NonExistentTags_ShouldReturnASetOfTags() {
-        // GIVEN
-        tagRepository.save(tags[1]);
-
-        // WHEN
-        var result = resolveExistentTags.resolve(tags);
-
-        // THEN
-        Assertions.assertTrue(result.contains(tags[0]));
-        Assertions.assertTrue(result.contains(tags[1]));
-        Assertions.assertEquals(2, result.size());
-    }
-
-    @Test
-    @DisplayName("Should return a set of tags when all tags are non existent")
-    void resolve_AllNonExistentTags_ShouldReturnASetOfTags() {
-        // WHEN
-        var result = resolveExistentTags.resolve(tags);
-        tagRepository.saveAll(result);
-
-        // THEN
-        Assertions.assertEquals(2, result.size());
-        result.forEach(tag -> Assertions.assertNotNull(tag.getId()));
-        Assertions.assertTrue(result.stream().anyMatch(tag -> tag.getName().equals(tags[0].getName())));
-    }
+   
 }
