5bfe1e20bd7d6873082dce8e5d5fd706f8455af2
lucasqueiroz <sepulchrolucas@gmail.com>
2023-11-03 15:09:48 -0300

    Add check to exclude events where the player is already a participant
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/br/hobbie/modules/event/domain/repositories/EventRepository.java
+++ src/main/java/com/br/hobbie/modules/event/domain/repositories/EventRepository.java
@@ -14,6 +14,9 @@ public interface EventRepository extends CrudRepository<Event, Long> {
                 select e from Event e join e.categories c where c.id in (
                     select t.id from Player p join p.interests t where p.id = :playerId
                 ) and e.active = true and e.admin.id != :playerId and e.startDate > current_date
+                and e.id not in (
+                    select e.id from Event e join e.participants p where p.id = :playerId
+                )
             """)
     Collection<Event> findAllMatchingWithInterests(Long playerId);
 
