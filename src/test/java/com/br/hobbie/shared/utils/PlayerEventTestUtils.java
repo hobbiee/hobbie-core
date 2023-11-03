c65be3d51b3d82ad4c5b5615a5e5efa4c61dc08f
lucasqueiroz <sepulchrolucas@gmail.com>
2023-11-02 22:36:30 -0300

    Add RequestExpiration functional interface and improve test cases
    Utility classes to be used on tests, implement all unit test cases for the `JoinRequest` entity
    
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

---
+++ src/test/java/com/br/hobbie/shared/utils/PlayerEventTestUtils.java
@@ -0,0 +1,32 @@
+package com.br.hobbie.shared.utils;
+
+import com.br.hobbie.modules.player.domain.entities.Player;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class PlayerEventTestUtils {
+    public static <T> T extractSomeField(Class<?> clazz, String fieldName, Object instance, Class<T> type) {
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return type.cast(field.get(instance));
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void assignId(Player player, Long value) {
+        Arrays.stream(player.getClass().getDeclaredFields()).forEach(field -> {
+            field.setAccessible(true);
+            if (Objects.equals(field.getName(), "id")) {
+                try {
+                    field.set(player, value);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
