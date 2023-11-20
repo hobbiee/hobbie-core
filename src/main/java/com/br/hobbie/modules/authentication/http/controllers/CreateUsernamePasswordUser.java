b9160df6d5278b89ce88c51e690b4cdd449980dd
lucasqueiroz <sepulchrolucas@gmail.com>
2023-11-20 01:53:37 -0300

    Update CreateUsernamePasswordUser to enable users by default and send email verification upon successful user creation
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/br/hobbie/modules/authentication/http/controllers/CreateUsernamePasswordUser.java
+++ src/main/java/com/br/hobbie/modules/authentication/http/controllers/CreateUsernamePasswordUser.java
@@ -1,6 +1,5 @@
 package com.br.hobbie.modules.authentication.http.controllers;
 
-import com.br.hobbie.modules.authentication.domain.entities.AuthenticationActions;
 import com.br.hobbie.modules.authentication.http.dtos.request.CreateUserRequest;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -8,20 +7,25 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/v1/api/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class CreateUsernamePasswordUser {
 
     @PersistenceContext
@@ -37,9 +41,7 @@ public class CreateUsernamePasswordUser {
         userRepresentation.setFirstName(request.firstName());
         userRepresentation.setLastName(request.lastName());
         userRepresentation.setEmailVerified(false);
-        // users remains disabled until email verification
-        userRepresentation.setEnabled(false);
-        userRepresentation.setRequiredActions(List.of(AuthenticationActions.VERIFY_EMAIL.action()));
+        userRepresentation.setEnabled(true);
         userRepresentation.setCredentials(request.credentials());
         return userRepresentation;
     }
@@ -49,10 +51,28 @@ public class CreateUsernamePasswordUser {
     public ResponseEntity<?> handle(@Valid @RequestBody CreateUserRequest request) {
         var userRepresentation = getUserRepresentation(request);
         var userEntity = request.toModel();
-        var response = keycloak.realm(realm).users().create(userRepresentation);
+        UsersResource usersResource = keycloak.realm(realm).users();
+        var response = usersResource.create(userRepresentation);
 
         if (response.getStatusInfo().getFamily().equals(Response.Status.Family.SUCCESSFUL)) {
             manager.persist(userEntity);
+            List<UserRepresentation> userRepresentationList = usersResource.searchByEmail(request.email(), true);
+
+            if (!CollectionUtils.isEmpty(userRepresentationList)) {
+                log.info("Sending email verification to user {}", request.email());
+                userRepresentationList
+                        .stream()
+                        .filter(user -> Objects.equals(false, user.isEmailVerified()))
+                        .findFirst()
+                        .ifPresent(user -> {
+                            log.info("User id {}", user.getId());
+                            usersResource.get(user.getId())
+                                    .sendVerifyEmail();
+                        });
+
+            }
+
+
             return ResponseEntity.created(response.getLocation()).build();
         }
 
