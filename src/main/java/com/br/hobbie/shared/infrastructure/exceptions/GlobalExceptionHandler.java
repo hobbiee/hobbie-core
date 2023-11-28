d60733e626b667d12e357a477e8603a0ae38d977
lucasqueiroz <sepulchrolucas@gmail.com>
2023-11-28 17:54:15 -0300

    Handle `UsernameNotFoundException` in GlobalExceptionHandler.
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- src/main/java/com/br/hobbie/shared/infrastructure/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/br/hobbie/shared/infrastructure/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.br.hobbie.shared.infrastructure.exceptions;
 
-import com.br.hobbie.shared.infrastructure.exceptions.validation_error.ValidationException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -30,10 +32,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .map(FieldError::getField)
                 .distinct()
                 .collect(Collectors.joining(", "));
-        var messages = fieldErrors
-                .stream()
-                .map(FieldError::getDefaultMessage)
-                .toArray();
+
 
         var exception = new ValidationException(
                 "Bad Request Exception. Invalid fields. Check the api docs.",
@@ -48,5 +47,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
 
         return ResponseEntity.badRequest().body(exception);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<AuthenticationExceptionResponse> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        var exception = new AuthenticationExceptionResponse(
+                "Sorry we could not authnticate you. Check your credentials and try again.",
+                HttpStatus.UNAUTHORIZED.value(),
+                ex.getMessage(),
+                Instant.now(),
+                ex.getClass().getName()
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception);
     }
 }
